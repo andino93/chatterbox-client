2084570775eb94d1638df435820bdad4a490138d
andino93 <andy.andino93@gmail.com>
2017-10-21 17:13:51 -0700

    finshed sprint

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,31 @@
 let app = {}; // container for app methods
-let JSONfetch; // storage for app.fetch()
+// storage for app.fetch()
+let chatroomContents;
+let currentRoom = 'lobby';
+let friends = {};
+
+app.chatrooms = {};
 
 app.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
 
 app.init = function() {
+  $('#chats').on('click','h2', function() {
+    let username = $(this).text();
+    friends[username.slice(0, username.length - 1)] = username.slice(0, username.length - 1);
+    app.handleUsernameClick(friends[username.slice(0, username.length - 1)]);
+  });
+  
+  $('.submit').on('click', function() {
+    app.handleSubmit($('#message').val());
+  });
+  
+  $('#roomSelect').on('change', function() {
+    currentRoom = $(this).children("option").filter(":selected").text();
+    app.fetch();
+    // currentRoom
+  });
+  
   app.fetch();
-  $('.username').on('click', app.handleUsernameClick($(this)));
 };
 
 app.send = function (message) {
@@ -30,13 +50,17 @@ app.fetch = function() {
   $.ajax({
     type: 'GET',
     url: app.server,
-    data: "order=-createdAt",
+    data: 'order=-createdAt&limit=500',
     contentType: 'application/json',
     success: function (data) {
-      _.each(data.results, function(messageObj) {
-        app.renderMessage(messageObj);
-      });
+      // move out of fetch function into own helper
+      // implement filter before passing into render messagez
+      app.receiveJSON(data.results);
+      // _.each(data.results, function(messageObj) {
+      //   app.renderMessage(messageObj);
+      // });
       JSONfetch = data;
+      
     },
     error: function (data) {
       console.log('failed attempt');  
@@ -48,45 +72,103 @@ app.fetch = function() {
   // console.log(message);
 };
 
+app.receiveJSON = function(dataArray) {
+  chatroomContents = app.filterResult(dataArray);
+  app.clearMessages();
+  _.each(dataArray, function(messageObj) {
+    app.populateChatrooms(messageObj);
+    if (messageObj.roomname === currentRoom) {
+      app.renderMessage(messageObj);
+    }
+    // app.populateChatrooms(messageObj);
+  });
+  // console.log(data)
+  // console.log(app.chatrooms);
+  // app.populateChatrooms();
+  
+};
+
+app.handleUsernameClick = function(username) {
+  // needs add user as a friend
+  // console.log(message);
+  $('.friends').append(`<p>${username}</p>`);
+};
+
 app.clearMessages = function() {
   $('#chats').empty();
 };
 
 app.renderMessage = function(messageObj) {
-  $('#chats').append(`<p>${messageObj.username}: ${messageObj.text}</p>`);
-};
-
-app.renderRoom = function(roomname) {
-  // let input = message.roomname || roomname;
-  $('#roomSelect').append(`<option>${roomname}</option>`);
-};
-
-app.handleUsernameClick = function(message) {
-  // needs add user as a friend
-  // console.log(message);
+  if (messageObj.text !== undefined || messageObj.username !== undefined) {
+    let message = _.escape(messageObj.text);
+    let username = _.escape(messageObj.username);
+  
+    $('#chats').append(`<h2 class="username">${username}:</h2><p>${message}</p>`);
+  }
+  // let username = _.escape(messageObj.username);
 };
 
 app.messageFormatter = function(message, username, chatroom) {
   return {
     username: username,
     text: message,
-    chatroom: chatroom
+    roomname: chatroom
   };
 };
 
 app.handleSubmit = function(message, username, chatroom) {
-  username = messageObj.username || window.location.search.slice(10);
-  chatroom = messageObj.chatroom || 'lobby';
-  let newMessage = messageFormatter(message, username, chatroom);
+  username = message.username || window.location.search.slice(10);
+  chatroom = message.roomname || currentRoom;
+  let newMessage = app.messageFormatter(message, username, chatroom);
 
   app.send(newMessage);
+  $('#message').val('');
+  app.fetch();
+};
 
+app.populateChatrooms = function(data) {
+  let chatroom = _.escape(data.roomname) || 'lobby';
+    if (!app.chatrooms.hasOwnProperty(chatroom)) {
+    // if key is !undefined
+    // add room to list
+    // append roomname to list
+    app.chatrooms[chatroom] = chatroom;
+    $('#roomSelect').append(`<option class="${data.roomname}">${data.roomname}</option>`);
+  }
+};
+
+app.switchChatroom = function(chatroomName) {
+  if (chatroomContents[chatroomName]) {
+    app.clearMessages();
+    _.each(chatroomContents[chatroomName], function(messageObj) {
+      app.renderMessage(messageObj);
+    });
+  }
+};
+
+app.filterResult = function(object) {
+  // debugger
+  return object.reduce(function(roomSelect, messageObj) {
+    if (!roomSelect.hasOwnProperty(messageObj.roomname)) {
+      roomSelect[messageObj.roomname] = [];
+    } 
+    
+    roomSelect[messageObj.roomname].push(messageObj);
+    
+    return roomSelect;
+  }, {});
 };
 
 $(document).ready(function() {
-  app.init(); // do stuff
-  $('.submit').on('click', function() {
-    app.handleSubmit($('#message').val())
-  });
+  // do stuf
+  app.init();
+
+  
+  setInterval(function() {
+    app.fetch();
+  }, 5000);
 
 });
+
+
+
